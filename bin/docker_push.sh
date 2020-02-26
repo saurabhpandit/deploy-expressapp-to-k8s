332d9288bcab853c63428baa5fc96a477e822e50
Saurabh Pandit <saurabh.c.pandit@gmail.com>
2020-02-26 22:36:32 +1100

    fixing deploy script

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker images
 
-if [[ -z "${TRAVIS_TAG}" ]]; then
+if [[ -z "$TRAVIS_TAG" ]]; then
     docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:latest
     docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:latest
 else
     echo "travis tag set"
-    docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:${TRAVIS_TAG}
-    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:${TRAVIS_TAG}
+    docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_TAG
+    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_TAG
 fi
