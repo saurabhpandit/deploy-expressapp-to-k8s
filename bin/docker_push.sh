b9e3dea7c0b1a050a8b20a7d07e07a2e55a80fd2
Saurabh Pandit <saurabh.c.pandit@gmail.com>
2020-02-26 22:02:10 +1100

    fixed travis.yml to not delete artifacts after build stage

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -5,9 +5,9 @@ docker images
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
     docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:latest
-    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER
+    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:latest
 else
     echo "travis tag set"
-    docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_TAG
-    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_TAG
+    docker tag $DOCKER_USERNAME/deploy-expressapp-to-k8s:$TRAVIS_BUILD_NUMBER $DOCKER_USERNAME/deploy-expressapp-to-k8s:${TRAVIS_TAG}
+    docker push $DOCKER_USERNAME/deploy-expressapp-to-k8s:${TRAVIS_TAG}
 fi
